6040066377c24865891edbb9479e739ae42444d8
unknown <gabriel_hidekii@hotmail.com>
2020-10-17 14:15:46 -0300

    Workshop 04

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
+
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Routes />
+    </>
+  );
+};
+
+export default App;
